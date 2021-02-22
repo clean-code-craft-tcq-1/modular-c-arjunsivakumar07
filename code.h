4ac9b9ccf2c7d9c491f0c1f5a382ae94e15ed0aa
arjunsivakumar07 <77655050+arjunsivakumar07@users.noreply.github.com>
2021-02-22 10:53:13 +0530

    Add files via upload

---
+++ code.h
@@ -0,0 +1,61 @@
+#ifndef code_H
+#define code_H
+
+/******************************************/
+/* Defines */
+/******************************************/
+
+
+/******************************************/
+/* Globar variables */
+/******************************************/
+const char* MajorColorNames_au8[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+
+const char* MinorColorNames_au8[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+
+int numberOfMajorColors_u8 =
+    sizeof(MajorColorNames_au8) / sizeof(MajorColorNames_au8[0]);
+
+
+int numberOfMinorColors_u8 =
+    sizeof(MinorColorNames_au8) / sizeof(MinorColorNames_au8[0]);
+	
+/******************************************/
+/* Enums */
+/******************************************/
+
+enum MajorColor_ten {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor_ten {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+
+/******************************************/
+/* Structs */
+/******************************************/
+typedef struct {
+    enum MajorColor_ten majorColor_en;
+    enum MinorColor_ten minorColor_en;
+} ColorPair_tst;
+
+
+/******************************************/
+/* Prototypes */
+/******************************************/
+void ColorPairToString(const ColorPair_tst* colorPair_st, char* buffer_au8);
+ColorPair_tst GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair_tst* colorPair_st);
+void testNumberToPair(int pairNumber,
+    enum MajorColor_ten expectedMajor,
+    enum MinorColor_ten expectedMinor);
+void testPairToNumber(
+    enum MajorColor_ten major,
+    enum MinorColor_ten minor,
+    int expectedPairNumber);
+
+
+#endif
---
+++ display.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "code.h"
+
+void ColorPairToString(const ColorPair_tst* colorPair_st, char* buffer_au8) {
+    sprintf(buffer_au8, "%s %s",
+        MajorColorNames_au8[colorPair_st->majorColor_en],
+        MinorColorNames_au8[colorPair_st->minorColor_en]);
+}
