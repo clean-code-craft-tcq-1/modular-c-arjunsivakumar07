31ab67743a085c381c8f9137ec0e1da50591e1b2
arjunsivakumar07 <77655050+arjunsivakumar07@users.noreply.github.com>
2021-02-23 23:25:13 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 #include "Test.h"
+
+void manual(void)
+{
+    int majorcolor, minorcolor;
+    ColorPair colorpair;
+    for(majorcolor =0; majorcolor < MajorColor_TotalNumber ; majorcolor++)
+    {
+        for(minorcolor =0; minorcolor < MinorColor_TotalNumber ; minorcolor++)
+        {
+            colorpair.majorColor = ((MajorColor_tst)majorcolor);
+            colorpair.minorColor = ((MinorColor_tst)minorcolor);
+            printf(" %s %s %d \n",MajorColorToString(colorpair.majorColor),MinorColorToString(colorpair.minorColor),GetPairNumberFromColor(&colorpair));
+        }
+    }
+}
+
 int main()
 {
   
@@ -8,6 +24,7 @@ int main()
 
     testPairToNumber(MajorColor_BLACK, MinorColor_ORANGE, 12);
     testPairToNumber(MajorColor_VIOLET, MinorColor_SLATE, 25);
-
+  
+    manual();
     return 0;
 }
