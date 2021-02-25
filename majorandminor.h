2381868fe6cedcd6c977cdef872c362d94fbf1c2
arjunsivakumar07 <77655050+arjunsivakumar07@users.noreply.github.com>
2021-02-25 09:06:02 +0530

    Update majorandminor.h

--- majorandminor.h
+++ majorandminor.h
@@ -1,23 +1,9 @@
 #ifndef MAJOR_AND_MINOR_H_
 #define MAJOR_AND_MINOR_H_
 
-typedef enum {
-    MajorColor_WHITE ,
-    MajorColor_RED,
-    MajorColor_BLACK,
-    MajorColor_YELLOW,
-    MajorColor_VIOLET,
-    MajorColor_TotalNumber
-    } MajorColor_tst;
+typedef enum {WHITE,RED,BLACK,YELLOW,VIOLET,Major_TotalNumber} MajorColor_tst;
     
-typedef enum {
-    MinorColor_BLUE,
-    MinorColor_ORANGE, 
-    MinorColor_GREEN, 
-    MinorColor_BROWN,
-    MinorColor_SLATE,
-    MinorColor_TotalNumber
-    } MinorColor_tst;
+typedef enum {BLUE,ORANGE, GREEN, BROWN,SLATE,Minor_TotalNumber} MinorColor_tst;
 
 #define MINORCOLORNAME_MAXCHARS     ((int)6)
 extern const char * MinorColorToString(MinorColor_tst minorcolor);
